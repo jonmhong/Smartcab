9367ed273746496ebdf39ab00c8f7a7822663f91
Jonathan Hong <jonmhong@gmail.com>
2016-06-10 13:38:55 -0700

    Correctly implemented q-learning equation
    Solved a nested dictionary bug, where all the values were being updated simultaneously.
    Redid the agent.py code.

--- agent.py
+++ agent.py
@@ -14,87 +14,84 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.q_table = {}
         
-        initialize_q = 12
         self.possible_actions = {}
         for move in self.env.valid_actions:
-            self.possible_actions[move] = initialize_q
+            self.possible_actions[move] = 12
+            
+        print "initialize: self.possible_actions: {}".format(self.possible_actions)
         
-        self.breaking_law_tally = 0
-        self.total_moves = 0
+        self.previous_action = None
+        self.previous_state = None
+        
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-
         self.destination = self.planner.destination
         # objective: choose what variables are necessary in the state
         
         
-    def update(self, t):
+    def update(self, t):   
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-        
-        previous_state = self.state
-        
+                
         # TODO: Update state
-        # create state entry in q_table as the agent explores a new state
         self.location = self.env.agent_states[self]['location']
         self.heading = self.env.agent_states[self]['heading']
         self.light = self.env.sense(self)['light']
-        self.state = (self.location, self.heading, self.light)
+        state = (self.location, self.next_waypoint, self.light) # add cars later
+
+        # adjust these parameters
+        gamma = 0.9 # discounting rate
+        epsilon = 0.1 # chance of choosing a random action
         
+        if t == 0:
+            alpha = 1
+        else:
+            alpha = 1.0 / t        
         
-        # tweak these variables
-        gamma = 0.5 # discounting rate 
-        alpha = 0.1 # learning rate to update q-value
-        epsilon = 0.05 # chance of choosing a random action for simulated annealing
+        #BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG
+        # there's a bug with updating nested dictionary values
         
-        if self.state is not None and self.state not in self.q_table:
-            self.q_table[self.state] = self.possible_actions
-        #else:
-            # TODO*: update q-table rewards, according to state and action
-        #    pass
-        
+        if (state is not None) and (state not in self.q_table):
+            # set the q-values to zero
+            self.q_table[state] = {'forward': 0, 'left': 0, 'right': 0, None: 0}
+
         # TODO: Select action according to your policy
         action = None
         
-        for move, q_value in self.q_table[self.state].iteritems():
-            max_q_value = max(self.q_table[self.state].values())
-            if q_value == max_q_value and max_q_value > 0:
-                policy = move
-                if random.random() > epsilon:
-                    action = move
-                else:
-                    action = random.choice(self.env.valid_actions)
-            else:
-                action = random.choice(self.env.valid_actions)
+        if random.random() > epsilon and (0 not in self.q_table[state].values()):
+            action = max(self.q_table[state])
+        else:
+            action = random.choice(self.possible_actions.keys())
         
-
-        # Execute action and get reward
         reward = self.env.act(self, action)
-        self.q_table[self.state][action] = reward
         
-        # set alpha, then decay it over time
-        
-
         # TODO: Learn policy based on state, action, reward
-        # utility function
-        if previous_state is not None:
-            if (previous_state[2] == 'red') and (self.state[0] != previous_state[0]):
-                self.breaking_law_tally += 1
-        self.total_moves += 1
+        q_val_f = self.q_table[state]['forward']
+        q_val_l = self.q_table[state]['left']
+        q_val_r = self.q_table[state]['right']
+        q_val_n = self.q_table[state][None]
+        q_hat = None
         
+        max_q = max([q_val_f, q_val_l, q_val_r, q_val_n])
         
-        #q_hat = ((1 - alpha) * self.q_table[state][action]) + (alpha * (gamma * sum(policy)))
-        
-        print "previous state: {}".format(previous_state)
-        print "current state: {}".format(self.state)
-        print "illegal moves tally: {}".format(self.breaking_law_tally)
-        print "total moves: {}".format(self.total_moves)
+        if self.q_table[state][action] == 0:
+            # if the state, action has never been taken, reward == q-value
+            self.q_table[state][action] = reward
+        else:
+            q_hat = ((1 - alpha) * self.q_table[self.previous_state][action]) + (alpha * (reward + gamma * max_q))
+            self.q_table[self.previous_state][action] = q_hat
+                
+        self.previous_state = state
         
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        print self.q_table
+        print self.possible_actions
+        print action
+        print state
 
 
 def run():
@@ -103,10 +100,10 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
+    sim = Simulator(e, update_delay=0.1)  # reduce update_delay to speed up simulation
     sim.run(n_trials=10)  # press Esc or close pygame window to quit
 
 
