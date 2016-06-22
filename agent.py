b1127afbe3b96cc2b015a2bb147a0287f63687bb
Jonathan Hong <jonmhong@gmail.com>
2016-06-22 11:55:04 -0700

    Feat: Added extra variables for analysis

--- agent.py
+++ agent.py
@@ -32,6 +32,8 @@ class LearningAgent(Agent):
         self.successful_runs = []
         self.success_tally = 0
         self.run_trial = 1
+        self.last_ten_trials = []
+        self.time_steps = []
         
         # created to analyze the rewards
         self.net_reward = 0
@@ -50,7 +52,7 @@ class LearningAgent(Agent):
         self.net_reward = 0
         
         
-    def update(self, t):   
+    def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
@@ -68,7 +70,7 @@ class LearningAgent(Agent):
         self.state = (self.next_waypoint, self.light)
         
         # creating and decaying parameters
-        self.gamma = 0.75 ** self.run_trial # discount rate
+        self.gamma = 0.82 ** self.run_trial # discount rate
         self.alpha = 1.0 / self.run_trial # learning rate
         self.epsilon = 1.0 / self.run_trial # randomness
         
@@ -87,21 +89,11 @@ class LearningAgent(Agent):
             action = random.choice(self.possible_actions.keys())
 
         reward = self.env.act(self, action)
-        self.net_reward += reward # this is used to check the net rewards at each trial
 
         # TODO: Learn policy based on state, action, reward
         q_hat = None
         max_q = max(self.q_table[self.state].values())
 
-        ### IMPLEMENT GAME THEORY
-        # optimize the policy by implementing game theory
-        # find the expected result in the situation
-        #if left or right or oncoming in ['left', 'right', 'oncoming']:
-            #agent needs to find a strategy based on 
-            # 1 if there's a car in the intersection
-            # 2 determine the maximin strategy in the intersection        
-        ### IMPLEMENT GAME THEORY
-        
         # calculate the q-value, as long as it is not in the initial state
         if self.previous_state is not None:
             # bellman equation
@@ -115,13 +107,38 @@ class LearningAgent(Agent):
         self.previous_state = self.state
         self.previous_action = action
 
+        # analysis for the last 10 trials
+        if self.run_trial > 90:
+            location = self.env.agent_states[self]['location']
+            destination = self.planner.destination
+            # this keeps track of the next waypoint, action, traffic light, cars in the intersection, agent's current location, and destination
+            # at each step, during the last 10 trials
+            self.last_ten_trials.append((self.next_waypoint, action, self.light, left, right, oncoming, location, destination))
+            self.net_reward += reward # this is used to check the net rewards at each trial
+            if self.env.done == True:
+                self.time_steps.append((self.run_trial, t, self.net_reward))
+
         # observing results for analysis and debugging
         if self.env.done == True:
-            self.success_tally += 1     
+            self.success_tally += 1
             self.successful_runs.append(self.run_trial)
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
         
+def plot_success(successful_runs):
+        x = range(0, 101)
+        y = np.zeros(len(x))
+        for x_coord in x:
+            if x_coord in successful_runs:
+                y[x_coord] = 1
+        pl.title("Successful Runs")
+        pl.hist(successful_runs, 
+                bins=len(x))
+        pl.xlabel("Trial Run Number")
+        pl.ylabel("Success (binary)")
+        pl.show()
+
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -135,20 +152,9 @@ def run():
     sim = Simulator(e, update_delay=0.0)  # reduce update_delay to speed up simulation
     sim.run(n_trials=100)  # press Esc or close pygame window to quit    
 
-    def plot_success(successful_runs):
-        x = range(0, 101)
-        y = np.zeros(len(x))
-        for x_coord in x:
-            if x_coord in successful_runs:
-                y[x_coord] = 1
-        pl.title("Successful Runs")
-        pl.hist(successful_runs, 
-                bins=len(x))
-        pl.xlabel("Trial Run Number")
-        pl.ylabel("Success (binary)")
-        pl.show()
-
     print a.q_table
+    print a.last_ten_trials
+    print a.time_steps
     print a.all_rewards
     print "List of trial numbers, where the agent successfully reached the destination:\n{}".format(a.successful_runs)
     print "Number of successful runs:\n{}".format(len(a.successful_runs))
